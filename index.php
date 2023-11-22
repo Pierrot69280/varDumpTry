96b8d972dad75c063cf574506295005c509beb89
Pierre Nogaro <pierre.nogaro@gmail.com>
2023-11-22 21:39:00 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// var_dump() nous permet d'afficher des informations détaillées sur une ou plusieurs variables.
+// y compris leur type et leur valeur.
+
+$prenom = "Pierre";
+$nom    = "Nogaro";
+$age    = 19;
+
+echo var_dump($prenom, $nom, $age);
+
+
+
